cf4be89adaaaa258198d6e5310ebc0823b14fe71
Thien Tran <gau.nernst@yahoo.com.sg>
2025-03-29 23:41:41 +0800

    add chat_hf.py

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ Gemma 3 provides FP32 checkpoints trained with INT4 Quantization Aware Training 
 No established libraries (in PyTorch) can take full advantage of the above. Hence, this library aims to do it.
 
 Plan:
-- Use tensor subclass to support tied embeddings
-- Safetensors-compatible serialization format. Transform plain tensors <-> tensor subclass
 - HF / vLLM / SGLang integration
 
 ```
+# convert checkpoint
 uv run convert_flax.py --ckpt_dir gemma3-1b-it-int4 --save_dir gemma-3-1b-it-int4
+
+# run chat demo
+python chat_hf.py --model gaunernst/gemma-3-1b-it-int4
 ```
---
+++ chat_hf.py
@@ -0,0 +1,61 @@
+import argparse
+
+import torch
+from safetensors.torch import load_file
+from torch import Tensor, nn
+from transformers import AutoTokenizer, Gemma3ForCausalLM, Gemma3TextConfig, TextStreamer, pipeline
+from transformers.modeling_utils import _get_resolved_checkpoint_files
+
+from subclass import from_plain
+
+
+def create_new_init(old_init):
+    def new_init(*args, **kwargs):
+        kwargs.update(device="meta")
+        old_init(*args, **kwargs)
+
+    return new_init
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", required=True)
+    args = parser.parse_args()
+
+    nn.Linear.__init__ = create_new_init(nn.Linear.__init__)
+    nn.Embedding.__init__ = create_new_init(nn.Embedding.__init__)
+
+    cfg = Gemma3TextConfig.from_pretrained(args.model)
+    model = Gemma3ForCausalLM(cfg)
+    Gemma3ForCausalLM.from_pretrained
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+    ckpts, _ = _get_resolved_checkpoint_files(
+        args.model, "", None, None, False, False, True, None, False, None, False, None, None, None, None
+    )
+    state_dict = dict()
+    for ckpt_path in ckpts:
+        print(f"Load checkpoint {ckpt_path}")
+        state_dict.update(load_file(ckpt_path))
+
+    state_dict = from_plain(state_dict)
+    missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False, assign=True)
+    assert len(missing_keys) == 1 and missing_keys[0] == "lm_head.weight"
+    assert len(unexpected_keys) == 0
+    model.tie_weights()
+
+    device = "cuda"
+    model.to(device).eval()
+    # TODO: figure out torch.compile
+    pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+    chat = []
+    while True:
+        chat.append(dict(role="user", content=input("User: ")))
+        response = pipe(chat, streamer=streamer, max_new_tokens=100_000)
+        chat = response[0]["generated_text"]
+
+
+if __name__ == "__main__":
+    main()
